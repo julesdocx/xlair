620541627eb46afd79304f87fecd862ef473b4ac
Jules Docx <jules.docx@gmail.com>
2024-10-16 15:11:52 +0200

    feat(page.tsx content): added construction icon and text. also changed favicon.ico and meta data title

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,17 @@ import LiveChat from './components/LiveChat';
 import Image from 'next/image';
 
 import logo from './assets/logo_white_full.png';
+import construction from './assets/construction.png';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image src={logo} alt="Xlair radio logo" width={288} />
-        <Image src={logo} alt="Xlair radio logo" className="blur-xl opacity-35 absolute" width={288} />
-        <div className="w-72 flex ">
-        <p className="max-w-72 text-xs tracking-tighter">Xl Air is a platform for anyone who likes to <span className="font-bold">experiment</span> with <span className="font-bold">sound</span>. It is an accessible multidisciplinary hotbed and it is also your playground.</p>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+        <Image src={logo} alt="Xlair radio logo" width={288} height="auto" />
+        <Image src={logo} alt="Xlair radio logo" className="blur-xl opacity-35 absolute" width={288} height="auto" />
+        <div className="flex text-xs items-center justify-center tracking-tighter"> <Image src={construction} alt="constructio emoji" width={30} height={10}/> <p className="p-2">xlair.be is currently under construction</p> <Image src={construction} alt="constructio emoji" width={30} height={10}/></div> 
+        <div className="w-72 flex flex-col">
+          <p className="max-w-72 text-xs tracking-tighter text-center">Xl Air is a platform for anyone who likes to <span className="font-bold">experiment</span> with <span className="font-bold">sound</span>. It is an accessible multidisciplinary hotbed and it is also your playground.</p>
         </div>
         <div className="w-72 justify-center flex flex-col gap-8 mt-6">
           <LiveRadioButton />
